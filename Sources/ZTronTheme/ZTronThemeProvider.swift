e96c1a873e7da6bcb0bbaa5eb9b39288659c9308
= <niccolo.dellarocca@gmail.com>
2025-07-01 10:51:00 +0200

    Added default theme

--- Sources/ZTronTheme/ZTronThemeProvider.swift
+++ Sources/ZTronTheme/ZTronThemeProvider.swift
@@ -14,4 +14,9 @@ public final class ZTronThemeProvider: ObservableObject {
     public final func replaceTheme(with other: any ZTronTheme) {
         self.theme = other.erasedToAnyTheme()
     }
+    
+    public static func `default`() -> any ZTronTheme {
+        return ZTronCommanderTheme()
+    }
+    
 }
