43e6bfcd89d092d1b8814ae765c90b789e27ef5d
= <niccolo.dellarocca@gmail.com>
2025-07-01 11:10:48 +0200

    Published UIColor init

--- Sources/ZTronTheme/Extension/UIColor/UIColor + fromTheme.swift
+++ Sources/ZTronTheme/Extension/UIColor/UIColor + fromTheme.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-internal extension UIColor {
+public extension UIColor {
     static func fromTheme(_ `set`: any ZTronColor, color: KeyPath<AnyZTronColor, String>) -> UIColor {
         return UIColor(named: set.erasedToAnyZTronColor()[keyPath: color], in: .module, compatibleWith: .current)!
     }
